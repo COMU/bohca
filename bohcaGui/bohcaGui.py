112c15143acca3d61352995533fb32c80a204980
mehtapgundogan <mehtapgundogan@gmail.com>
2012-12-19 20:16:49 +0200

    notification dosy

--- bohcaGui/bohcaGui.py
+++ bohcaGui/bohcaGui.py
@@ -31,7 +31,7 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
        
     def showChange(self):
        
-       logging.basicConfig(filename="/home/mehtap/bohcaaaa.log",level=logging.INFO,
+       logging.basicConfig(filename="/home/mehtap/bohca.log",level=logging.INFO,
                            format='%(asctime)s - %(message)s-',
                            datefmt='%Y-%m-%d %H:%M:%S')
        
