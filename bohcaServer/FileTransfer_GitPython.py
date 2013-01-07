891c00a9933664f0c2fd1470137f797ba9c9c230
kancer ezeroglu <ezeroglukancer@gmail.com>
2013-01-07 20:18:35 +0200

    bir kaç düzenleme yapıldı. Dosyanın depoya gönderilme işlemi doğru çalışıp çalışmadığının kontrolü yapılacak.

--- bohcaServer/FileTransfer_GitPython.py
+++ bohcaServer/FileTransfer_GitPython.py
@@ -9,16 +9,10 @@ depo_register_file = open("/home/kancer/bohca/.git/config","r")
 dizi = depo_register_file.readlines()
 
 if  '[remote "'+select_repo+'"]\n' in dizi:
-	logging.info("this repo have been created. enter new repo name!")
+	logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+	logging.info("this repo have been created. if you want create new repo, you must enter new repo name!")
 else:
-		test_remote = repo.create_remote(select_repo,'git@github.com:COMU/bohca.git') # create repo
-
-current_index = os.getcwd
-path = current_index + "/.git"
-print(path)
-
-os.chdir(path) #change working directory
-file_config = open("config","r")
+	test_remote = repo.create_remote(select_repo,'git@github.com:COMU/bohca.git') # create repo
 
 def exit_program():
         print "exit the program"
@@ -28,56 +22,45 @@ def exit_program():
 while True:
 
 	commit_message = raw_input("Enter your commit message: ")
-
-	choose = str(raw_input("If you want choose a file from your PC, you enter (B/b): "))
-	if choose == 'B' or choose == 'b':
-		
-		
-		os.system("xdg-open /home/kancer/bohca")
-
-	elif choose != 'B' or choose != 'b':
-        	file_name = raw_input("Enter the file name:")
-        	if file_name:
-                	try:
-                        	f = open(file_name, "r")
-                        	logging.warn('file was found')
+	#choose = str(raw_input("If you want choose a file from your PC, you enter (B/b): "))
+        
+	file_name = raw_input("Enter the file name:")
+        if file_name:
+        	try:
+                	f = open(file_name, "r")
+                        logging.warn('file was found')
                         
-				#dosyanin var olup olmadiginin kontrolu
-                        	path = os.path.abspath(file_name)
-                        	flag = os.path.isfile(path)
-                        	if flag == True:
-                                	logging.warn('file has been had')
+			#dosyanin var olup olmadiginin kontrolu
+			logging.basicConfig(format='%(message)s %(filename)s', level=logging.DEBUG)
+                        path = os.path.abspath(file_name)
+                        flag = os.path.isfile(path)
+                        if flag == True:
+                                logging.warn('file has been had')
 
-                	except IOError:
-                         	logging.warn('file not found')
-                         	open(file_name, "w")
+                except IOError:
+                         logging.warn('file not found')
+                         open(file_name, "w")
 								
-				logging.basicConfig(format='%(message)s %(filename)s', level=logging.DEBUG)
-				
-				logging.info('created new file the name of')
-				
-			
+			 logging.basicConfig(format='%(message)s %(filename)s', level=logging.DEBUG)
+		 	 logging.info('created new file the name of '+"'"+file_name+"'")
+			 exit_program()
 
-                	exit_program()
+		index = select_repo.index
+        	file_add = index.add([file_name])
+        	new_commit = index.commit(commit_message)
+        	file_add.push(file_name)
+        	new_commit.push()
 
-        	else:
-                	 logging.warn('Please specify the file name!')
+        	devel = repo.remotes.devel # get default remote by name
+        	ref = devel.refs
+        	d = devel.name
+        	d.fetch()
+        	d.pull()
+        	d.push()
 
-		
+        	print "references: %s" %ref
 
-		index = repo.index
-		file_add = index.add([file_name])
-		new_commit = index.commit(commit_message)
-		file_add.push(file_name)
-		new_commit.push()
+        	file_name.close()
 	
-		devel = repo.remotes.devel # get default remote by name
-		ref = devel.refs
-		d = devel.name
-		d.fetch()
-		d.pull()
-		d.push()
-
-		print "references: %s" %ref
-		
-		file_name.close()
+        else:
+        	logging.warn('Please specify the file name!')
