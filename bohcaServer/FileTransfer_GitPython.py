037a408dd1e507078ca6039f35a2cc7fef995e24
kancer ezeroglu <ezeroglukancer@gmail.com>
2013-01-09 12:28:49 +0200

    kodda bir kaç düzeltme yapıldı. eksik olan yerler yorum satırlarıyla belirtildi.

--- bohcaServer/FileTransfer_GitPython.py
+++ bohcaServer/FileTransfer_GitPython.py
@@ -6,40 +6,45 @@ assert repo.bare == False
 
 select_repo = str(raw_input("Enter repo name:"))
 
-def check_repo():
-        if  '[remote "'+select_repo+'"]\n' in dizi:
-                if repo_is_empty() == True:
-                        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-                        logging.info("this repo have been created.")
-                        choose = str(raw_input("if you want create new repo enter(C/c) or you can pass"))
-                        if choose == "C" | choose == "c":
-                                select_repo = str(raw_input("Enter repo name:"))
-                                check_repo()
-                        cloned_repo = repo.clone("/home/kancer/"+select_repo)
-                else:
-                        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-                        logging.info("this repo is not empyt. please select another repo!")
-                        select_repo = str(raw_input("Enter repo name:"))
-                        check_repo()
-        else:
-                logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-                logging.info("the name of repo is"+"'"+select_repo+"'"+"which is created")
-                test_remote = repo.create_remote(select_repo,'git@github.com:COMU/bohca.git') # create repo
-                cloned_repo = repo.clone("/home/kancer/"+select_repo)
-
-check_repo()
-
-def repo_is_empty():
-#klasor ici bos mu degil mi kontrolu
-	direc_inc = os.listdir("/home/kancer"+select_repo)
-	print direc_inc
-	if direc_inc == " ":
-		return True
-	else:
-		return False
-
 repo_register_file = open("/home/kancer/bohca/.git/config","r")
 dizi = repo_register_file.readlines()
+
+#check repo is empty or full
+def repo_is_empty():
+        direc_inc = os.listdir("/home/kancer/"+select_repo)
+        print direc_inc
+        if direc_inc == " ":
+                return True
+        else:
+                return False
+
+#check repo is created in the past
+if  '[remote "'+select_repo+'"]\n' in dizi:
+	if repo_is_empty() == True:    # if repo which created in the past is empty
+		logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+                logging.info("this repo have been created.")
+                choose = str(raw_input("if you want create new repo enter(C/c) or you can pass"))
+                if choose == "C" | choose == "c":
+                	select_repo = str(raw_input("Enter repo name:"))
+			flag = repo_is_empty() # if user choose repo which created in the past, it is false for this program so we must check it.
+			if flag == True:  # if repo is empty
+				test_remote = repo.create_remote(select_repo,'https://github.com/COMU/bohca.git') # create repo
+			else:
+				logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+                		logging.info("this repo have been created. you must choose another repo name!")
+				#burada tekrar basa donup yeni bir depo sectirmesi gerekiyor
+
+                cloned_repo = repo.clone("/home/kancer/"+select_repo)
+        else:
+        	logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+                logging.info("this repo is not empty. please select another repo!")
+                #select_repo = str(raw_input("Enter repo name:"))
+		#burada tekrar depo sectirip deponun var olup olmadigi kontrol edilmeli
+else:
+	logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+        logging.info("the name of repo is "+"'"+select_repo+"'"+" is created")
+        test_remote = repo.create_remote(select_repo,'https://github.com/COMU/bohca.git') # create repo
+        cloned_repo = repo.clone("/home/kancer/"+select_repo)   #clone da hata olusuyor nedeeeennnnn.........
 
 def exit_program():
         print "exit the program"
@@ -49,8 +54,6 @@ def exit_program():
 while True:
 
 	commit_message = raw_input("Enter your commit message: ")
-	#choose = str(raw_input("If you want choose a file from your PC, you enter (B/b): "))
-        
 	file_name = raw_input("Enter the file name:")
         if file_name:
 		#dosyanin var olup olmadiginin kontrolu
@@ -71,7 +74,7 @@ while True:
 			 logging.basicConfig(format='%(message)s %(filename)s', level=logging.DEBUG)
 		 	 logging.info('created new file the name of '+"'"+file_name+"'")
 
-		index = select_repo.index
+		index = repo.index
         	file_add = index.add([file_name])
         	new_commit = index.commit(commit_message)
         	file_add.push(file_name)
