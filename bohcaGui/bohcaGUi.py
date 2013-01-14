5723384b8f14929bb6338da1705864186d6e0b43
mehtapgundogan <mehtapgundogan@gmail.com>
2013-01-14 13:44:10 +0200

    arayuz çalışır halde

--- bohcaGui/bohcaGUi.py
+++ bohcaGui/bohcaGUi.py
@@ -1,18 +1,11 @@
 import sys, os
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtGui import QSystemTrayIcon
-#import Settpro
-import File_Notify
 
-#project_path = os.environ('HOME')
-#print project_path
-#icon_path = project_path + '/bohca' 
-project_home_dir = "/home/mehtap/bohca"
-log_file = project_home_dir + "/bohca.log"
-log_file_copy = project_home_dir + "/bohcacopy.log"
-icon_path= project_home_dir + "/icons/Flock_icon.png"
-workspace_path = "/home/mehtap/Desktop/bohca/"
-BASEDIR = "/home/mehtap/Desktop/bohca/"
+
+project_path = os.getenv('HOME')+'/bohca'
+icon_path= project_path + "/icons/Flock_icon.png"
+
 
 
 
@@ -29,7 +22,7 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
        
        
         self.setContextMenu(menu)
-    
+        
 
     #exit actioninin cagirdigi fonksiyon
     def close(self):
@@ -45,9 +38,8 @@ def main():
 
     #trayicona bir simge verme
     trayIcon = SystemTrayIcon(QtGui.QIcon(icon_path),app_widget)
-    trayIcon.show()
-      
-
+    trayIcon.show()  
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
      main()
