491a1b20dfee30c2cc4cce1e9357cecdfc4b2c25
kancer ezeroglu <ezeroglukancer@gmail.com>
2013-02-17 20:01:48 +0200

    Yeni bir kullanıcı giris yaptığında o kullanıcıya ait sunucu da bir dizin ve dosya oluşturuldu. Bu dosya içerisine kullanıcı adı ve parolası kaydedildi.Kurulum ekranı oluşturuldu ve açılış ekranıyla bağlantılı hale getirildi #48 #16 #13

--- bohcaGui/SetupScreen/ui_openscreen.py
+++ bohcaGui/SetupScreen/ui_openscreen.py
@@ -9,7 +9,7 @@
 
 from PyQt4 import QtCore, QtGui
 import sys, os
-#from ui_mainwindow import * 
+import subprocess
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -18,77 +18,75 @@ except AttributeError:
 
 default_path = os.getenv('HOME')+'/Bohca'
 
-
-	        
-
 class Ui_MainWindow(QtGui.QMainWindow):
    
          
-     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        self.setObjectName(_fromUtf8("MainWindow"))
-        self.resize(612, 473)
-        self.centralwidget = QtGui.QWidget(self)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(342, 130, 201, 27))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(340, 190, 201, 27))
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 20, 171, 151))
-        self.label.setText(_fromUtf8(""))
-        self.label.setPixmap(QtGui.QPixmap(_fromUtf8("../../icons/Icon_Large_or_Small.png")))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(220, 130, 111, 21))
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.label_3 = QtGui.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(220, 190, 101, 21))
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(310, 360, 98, 27))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../../icons/gif_47_089.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(460, 360, 98, 27))
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("../../icons/gif_47_023.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton_2.setIcon(icon1)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.label_4 = QtGui.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(250, 40, 261, 31))
-        self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.label_5 = QtGui.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(240, 260, 311, 20))
-        self.label_5.setObjectName(_fromUtf8("label_5"))
-        self.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(self)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 612, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        self.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(self)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        self.setStatusBar(self.statusbar)
+	def __init__(self):
+        	QtGui.QMainWindow.__init__(self)
+        	self.setObjectName(_fromUtf8("MainWindow"))
+        	self.resize(612, 473)
+        	self.centralwidget = QtGui.QWidget(self)
+        	self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        	self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        	self.lineEdit.setGeometry(QtCore.QRect(342, 130, 201, 27))
+        	self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        	self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
+        	self.lineEdit_2.setGeometry(QtCore.QRect(340, 190, 201, 27))
+      		self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+      	  	self.label = QtGui.QLabel(self.centralwidget)
+      	  	self.label.setGeometry(QtCore.QRect(20, 20, 171, 151))
+      	  	self.label.setText(_fromUtf8(""))
+      	  	self.label.setPixmap(QtGui.QPixmap(_fromUtf8("../../icons/Icon_Large_or_Small.png")))
+      	  	self.label.setObjectName(_fromUtf8("label"))
+      	  	self.label_2 = QtGui.QLabel(self.centralwidget)
+      	  	self.label_2.setGeometry(QtCore.QRect(220, 130, 111, 21))
+      	  	self.label_2.setObjectName(_fromUtf8("label_2"))
+      	  	self.label_3 = QtGui.QLabel(self.centralwidget)
+      	  	self.label_3.setGeometry(QtCore.QRect(220, 190, 101, 21))
+      	  	self.label_3.setObjectName(_fromUtf8("label_3"))
+      	  	self.pushButton = QtGui.QPushButton(self.centralwidget)
+      	  	self.pushButton.setGeometry(QtCore.QRect(310, 360, 98, 27))
+      	  	icon = QtGui.QIcon()
+      	  	icon.addPixmap(QtGui.QPixmap(_fromUtf8("../../icons/gif_47_089.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+      	  	self.pushButton.setIcon(icon)
+      	  	self.pushButton.setObjectName(_fromUtf8("pushButton"))
+      	  	self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+      	  	self.pushButton_2.setGeometry(QtCore.QRect(460, 360, 98, 27))
+      	  	icon1 = QtGui.QIcon()
+      	  	icon1.addPixmap(QtGui.QPixmap(_fromUtf8("../../icons/gif_47_023.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+      	  	self.pushButton_2.setIcon(icon1)
+      	  	self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+      	  	self.label_4 = QtGui.QLabel(self.centralwidget)
+      	  	self.label_4.setGeometry(QtCore.QRect(250, 40, 261, 31))
+      	  	self.label_4.setObjectName(_fromUtf8("label_4"))
+      	  	self.label_5 = QtGui.QLabel(self.centralwidget)
+      	  	self.label_5.setGeometry(QtCore.QRect(240, 260, 311, 20))
+      	  	self.label_5.setObjectName(_fromUtf8("label_5"))
+      	  	self.setCentralWidget(self.centralwidget)
+      	  	self.menubar = QtGui.QMenuBar(self)
+      	  	self.menubar.setGeometry(QtCore.QRect(0, 0, 612, 25))
+      	  	self.menubar.setObjectName(_fromUtf8("menubar"))
+      	  	self.setMenuBar(self.menubar)
+      	  	self.statusbar = QtGui.QStatusBar(self)
+      	  	self.statusbar.setObjectName(_fromUtf8("statusbar"))
+      	  	self.setStatusBar(self.statusbar)
 
-        self.retranslateUi(self)
-	
-        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.close)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.check)
-        QtCore.QMetaObject.connectSlotsByName(self)
+      	  	self.retranslateUi(self)
+      	  	
+      	  	QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.close)
+      	  	QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.check)
+      	  	QtCore.QMetaObject.connectSlotsByName(self)
 
-     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">User Name :</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Password :</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Next ->", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_4.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:22pt; font-weight:600;\">Welcome To Bohca</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_5.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" color:#ff0000;\">  Please enter your user name and password !</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-     def check(self):
+	def retranslateUi(self, MainWindow):
+        	MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label_2.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">User Name :</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label_3.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600;\">Password :</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        	self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Next ->", None, QtGui.QApplication.UnicodeUTF8))
+        	self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label_4.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-size:22pt; font-weight:600;\">Welcome To Bohca</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label_5.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" color:#ff0000;\">  Please enter your user name and password !</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+     
+	def check(self):
 		
 		user_name = str(ui.lineEdit.text())
 		user_password = str(ui.lineEdit_2.text())
@@ -101,12 +99,30 @@ class Ui_MainWindow(QtGui.QMainWindow):
 			ui.label_5.setText("<html><head/><body><p><span style=\" color:#ff0000;\">  You must filling fields !</span></p></body></html>")
 		else:
 			ui.label_5.setText("")
- #               next_object.path()
-               # next_object = SelectPath()
-                self.new_open()
-                self.close()
-     def new_open(self):
-                self.new = Ui_MainWindow2()
+
+			user_direc_name = user_name + "-bohca"
+			if user_direc_name:
+				try:
+					user_direc = bool(open(os.getenv('HOME')+ "/gitolite-admin/"+ user_direc_name+ "/"+ "kayit.txt","r"))
+					ui.label_5.setText("<html><head/><body><p><span style=\" color:#ff0000;\">  You had done setup Bohca !</span></p></body></html>")		
+			
+				except IOError:
+
+				# bu kısım web sayfasında kayıt sırasında yapılacak !
+					os.mkdir(os.getenv('HOME')+ "/gitolite-admin/" + user_direc_name)  # directory is created at this line.
+					file_user = open(os.getenv('HOME') + "/gitolite-admin/"+ user_direc_name+ "/"+ "kayit.txt","w")
+	
+						# dosyaya kullanıcı adı ve parolası eklencek
+					file_user.write(user_name + "&" + user_password)
+					file_user.close()  	
+					file_name = os.getenv('HOME') + "/bohca/bohcaGui/SetupScreen/ui_openscreen.py"
+					if subprocess.call(["python --first-time",file_name]): # HATALI bu kisim duzeltilmeli	
+                				self.new_open()
+                				self.close()
+					else:
+						ui.label_5.setText("<html><head/><body><p><span style=\" color:#ff0000;\">  You had done setup Bohca !</span></p></body></html>")
+	def new_open(self):
+        	self.new = Ui_MainWindow2()
                 self.new.show()                  
 
         
@@ -118,70 +134,70 @@ class Ui_MainWindow(QtGui.QMainWindow):
                                 
                
 class Ui_MainWindow2(QtGui.QMainWindow):
-    def __init__(self):
-        super(Ui_MainWindow2,self).__init__()
+	def __init__(self):
+        	super(Ui_MainWindow2,self).__init__()
 
-        self.setObjectName(_fromUtf8("MainWindow"))
-        self.resize(602, 339)
-        self.centralwidget = QtGui.QWidget(self)
-        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(310, 240, 98, 27))
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(_fromUtf8("icons/gif_47_089.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton.setIcon(icon)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(440, 240, 98, 27))
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(_fromUtf8("icons/gif_47_023.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.pushButton_2.setIcon(icon1)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(180, 130, 91, 31))
-        self.label.setObjectName(_fromUtf8("label"))
-        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(280, 130, 271, 27))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(10, 0, 171, 151))
-        self.label_2.setText(_fromUtf8(""))
-        self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("Icon_Large_or_Small.png")))
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.label_3 = QtGui.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(260, 40, 261, 51))
-        font = QtGui.QFont()
-        font.setPointSize(22)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.setCentralWidget(self.centralwidget)
-        self.menubar = QtGui.QMenuBar(self)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 25))
-        self.menubar.setObjectName(_fromUtf8("menubar"))
-        self.setMenuBar(self.menubar)
-        self.statusbar = QtGui.QStatusBar(self)
-        self.statusbar.setObjectName(_fromUtf8("statusbar"))
-        self.setStatusBar(self.statusbar)
+        	self.setObjectName(_fromUtf8("MainWindow"))
+        	self.resize(602, 339)
+        	self.centralwidget = QtGui.QWidget(self)
+        	self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        	self.pushButton = QtGui.QPushButton(self.centralwidget)
+        	self.pushButton.setGeometry(QtCore.QRect(310, 240, 98, 27))
+        	icon = QtGui.QIcon()
+        	icon.addPixmap(QtGui.QPixmap(_fromUtf8("icons/gif_47_089.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        	self.pushButton.setIcon(icon)
+        	self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        	self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        	self.pushButton_2.setGeometry(QtCore.QRect(440, 240, 98, 27))
+        	icon1 = QtGui.QIcon()
+        	icon1.addPixmap(QtGui.QPixmap(_fromUtf8("icons/gif_47_023.gif")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        	self.pushButton_2.setIcon(icon1)
+        	self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        	self.label = QtGui.QLabel(self.centralwidget)
+        	self.label.setGeometry(QtCore.QRect(180, 130, 91, 31))
+        	self.label.setObjectName(_fromUtf8("label"))
+        	self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        	self.lineEdit.setGeometry(QtCore.QRect(280, 130, 271, 27))
+        	self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        	self.label_2 = QtGui.QLabel(self.centralwidget)
+        	self.label_2.setGeometry(QtCore.QRect(10, 0, 171, 151))
+        	self.label_2.setText(_fromUtf8(""))
+        	self.label_2.setPixmap(QtGui.QPixmap(_fromUtf8("Icon_Large_or_Small.png")))
+        	self.label_2.setObjectName(_fromUtf8("label_2"))
+        	self.label_3 = QtGui.QLabel(self.centralwidget)
+        	self.label_3.setGeometry(QtCore.QRect(260, 40, 261, 51))
+        	font = QtGui.QFont()
+        	font.setPointSize(22)
+        	font.setBold(True)
+        	font.setWeight(75)
+        	self.label_3.setFont(font)
+        	self.label_3.setObjectName(_fromUtf8("label_3"))
+        	self.setCentralWidget(self.centralwidget)
+        	self.menubar = QtGui.QMenuBar(self)
+        	self.menubar.setGeometry(QtCore.QRect(0, 0, 602, 25))
+        	self.menubar.setObjectName(_fromUtf8("menubar"))
+        	self.setMenuBar(self.menubar)
+        	self.statusbar = QtGui.QStatusBar(self)
+        	self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        	self.setStatusBar(self.statusbar)
 
-        self.retranslateUi(self)
-        QtCore.QObject.connect(self.lineEdit, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.pushButton.click)
-        QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.close)
-        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.path)
-        QtCore.QMetaObject.connectSlotsByName(self)
+        	self.retranslateUi(self)
+        	QtCore.QObject.connect(self.lineEdit, QtCore.SIGNAL(_fromUtf8("returnPressed()")), self.pushButton.click)
+        	QtCore.QObject.connect(self.pushButton_2, QtCore.SIGNAL(_fromUtf8("clicked()")), self.close)
+        	QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.path)
+        	QtCore.QMetaObject.connectSlotsByName(self)
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Next ->", None, QtGui.QApplication.UnicodeUTF8))
-        self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Current Path:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Welcome to Bohca", None, QtGui.QApplication.UnicodeUTF8))
+	def retranslateUi(self, MainWindow):
+        	MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        	self.pushButton.setText(QtGui.QApplication.translate("MainWindow", "Next ->", None, QtGui.QApplication.UnicodeUTF8))
+        	self.pushButton_2.setText(QtGui.QApplication.translate("MainWindow", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label.setText(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:600;\">Current Path:</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        	self.label_3.setText(QtGui.QApplication.translate("MainWindow", "Welcome to Bohca", None, QtGui.QApplication.UnicodeUTF8))
 
-    def path(self):
-                self.lineEdit.setText(default_path)
-
-                path = str(self.lineEdit.text())
+    		self.lineEdit.setText(default_path)
+	def path(self):
+                
+		path = str(self.lineEdit.text())
                 self.lineEdit.clear()
                 if path == "":
                         if sys.platform == 'linux2':
@@ -203,9 +219,6 @@ class Ui_MainWindow2(QtGui.QMainWindow):
                                         subprocess.call(["xdg-open", file_path])
                         else:
                                 os.startfile(file_path)
-
-
-
 
 
 app = QtGui.QApplication(sys.argv)
