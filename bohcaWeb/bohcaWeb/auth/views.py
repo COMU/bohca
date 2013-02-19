009aa90c98b8c05f1316e1a08e6e459666ed454b
mehtapgundogan <mehtapgundogan@gmail.com>
2013-02-20 01:18:29 +0200

    bohca için basit bir web sayfası oluşturuldu,ancak hatalı. Related #35

---
+++ bohcaWeb/bohcaWeb/auth/views.py
@@ -0,0 +1,22 @@
+#auth/views.py
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active"
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('auth.html',{'state':state, 'username': username})
